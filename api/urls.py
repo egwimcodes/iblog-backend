9d7d2038abced7710386775feb0850b2bf1c6776
egwimcodes <egwimcodes@gmail.com>
2025-05-09 15:04:30 +0100

    .....

--- api/urls.py
+++ api/urls.py
@@ -2,6 +2,6 @@ from django.urls import path
 from .views import  CreateIBlogUserGCBV, AllIblogUser
 
 urlpatterns = [
-    path("users/", AllIblogUser.as_view(), name="all user"),
+    path("allusers/", AllIblogUser.as_view(), name="all user"),
     path("register/", CreateIBlogUserGCBV.as_view(), name='register')
 ]
