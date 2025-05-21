e8180adefc858482abf9e23435d6699dc4dc8706
egwimcodes <egwimcodes@gmail.com>
2025-05-21 17:54:03 +0100

    Blog Create endpoint done ...

--- api/urls.py
+++ api/urls.py
@@ -3,7 +3,7 @@ from .views import CreateIBlogUserGCBV, AllIblogUser, CreateBlogView, RetrieveUp
 
 urlpatterns = [
     path("register/", CreateIBlogUserGCBV.as_view(), name='register'),
-    path("users/", AllIblogUser.as_view(), name="all_user"),
+    path("allusers/", AllIblogUser.as_view(), name="all_user"),
     path("blogs/", CreateBlogView.as_view(), name='blogs'),
     path("blogs/<int:pk>/",RetrieveUpdateDestroyIBlogUserView.as_view(), name="get-rudibusv" ),
     path("blogs/<int:pk>/",
--- core/urls.py
+++ core/urls.py
@@ -3,6 +3,7 @@ from django.contrib import admin
 from django.urls import path, include
 from django.conf import settings
 from django.conf.urls.static import static
+
 from rest_framework_simplejwt.views import (
     TokenObtainPairView,
     TokenRefreshView,
@@ -13,8 +14,12 @@ urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/token/', TokenObtainPairView.as_view(), name='token_obtain_pair'),
     path('api/token/refresh/', TokenRefreshView.as_view(), name='token_refresh'),
-    path('api/', include('api.urls'))
-] 
+    path('api/', include('api.urls')), 
+    path('auth/', include('dj_rest_auth.urls')),
+    path('auth/registration/', include('dj_rest_auth.registration.urls')),
+    path('auth/', include('allauth.urls')),  # For social login
+]
 
 if settings.DEBUG:
-    urlpatterns +=  static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
+    urlpatterns += static(settings.MEDIA_URL,
+                          document_root=settings.MEDIA_ROOT)
